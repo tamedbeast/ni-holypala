80a83d6d73ebcaabd236cf7ce26029e92c945f50
nelbin4 <nelbinbinag@yahoo.com>
2023-06-19 07:19:53 +0800

    Update HolyPaladin.lua

--- HolyPaladin.lua
+++ HolyPaladin.lua
@@ -1,6 +1,7 @@
 local enables = {
     ["heal"] = true,
     ["offense"] = true,
+    ["buffs"] = true,
 }
 
 local values = {
@@ -11,7 +12,7 @@ local values = {
     ["divineIlluminationThreshold"] = 65, -- Set the default threshold for Divine Illumination to 65
     ["divineShieldThreshold"] = 35, -- Set the default threshold for Divine Shield to 35
     ["handOfProtectionThreshold"] = 40, -- Set the default threshold for Hand of Protection to 40
-    ["layOnHandsThreshold"] = 25, -- Adjust the threshold as desired	
+    ["layOnHandsThreshold"] = 25, -- Adjust the threshold as desired
 }
 
 local spells = {
@@ -23,6 +24,8 @@ local spells = {
     divineIllumination = 31842,
     divineShield = 642,
     handOfProtection = 10278,
+    beaconOfLight = 53563,
+    sacredShield = 53601,
 }
 
 local function GUICallback(key, item_type, value)
@@ -39,16 +42,19 @@ local items = {
     { type = "separator" },
     { type = "entry", text = "Enable Healing", tooltip = "Enable or disable automatic healing", enabled = true, key = "heal" },
     { type = "entry", text = "Aura Mastery Threshold", tooltip = "The health percentage at which to use Aura Mastery", value = 65, min = 0, max = 100, step = 1, key = "auraMasteryThreshold" },
-	{ type = "entry", text = "Divine Illumination Threshold", tooltip = "The mana percentage at which to use Divine Illumination", value = 65, min = 0, max = 100, step = 1, key = "divineIlluminationThreshold" },
-	{ type = "entry", text = "Divine Shield Threshold", tooltip = "The health percentage at which to use Divine Shield", value = 35, min = 0, max = 100, step = 1, key = "divineShieldThreshold" },
-	{ type = "entry", text = "Hand of Protection Threshold", tooltip = "The health percentage at which to use Hand of Protection", value = 40, min = 0, max = 100, step = 1, key = "handOfProtectionThreshold" },
-	{ type = "entry", text = "Lay on Hands Threshold", tooltip = "The health percentage at which to use Lay on Hands", value = 10, min = 0, max = 100, step = 1, key = "layOnHandsThreshold" },
+    { type = "entry", text = "Divine Illumination Threshold", tooltip = "The mana percentage at which to use Divine Illumination", value = 65, min = 0, max = 100, step = 1, key = "divineIlluminationThreshold" },
+    { type = "entry", text = "Divine Shield Threshold", tooltip = "The health percentage at which to use Divine Shield", value = 35, min = 0, max = 100, step = 1, key = "divineShieldThreshold" },
+    { type = "entry", text = "Hand of Protection Threshold", tooltip = "The health percentage at which to use Hand of Protection", value = 40, min = 0, max = 100, step = 1, key = "handOfProtectionThreshold" },
+    { type = "entry", text = "Lay on Hands Threshold", tooltip = "The health percentage at which to use Lay on Hands", value = 10, min = 0, max = 100, step = 1, key = "layOnHandsThreshold" },
     { type = "entry", text = "Divine Favor Threshold", tooltip = "The health percentage at which to use Divine Favor", value = 25, min = 0, max = 100, step = 1, key = "divineFavorThreshold" },
-	{ type = "separator" },
-	{ type = "entry", text = "Holy Shock Threshold", tooltip = "The health percentage at which to use Holy Shock", value = 88, min = 0, max = 100, step = 1, key = "holyShockThreshold" },
+    { type = "separator" },
+    { type = "entry", text = "Holy Shock Threshold", tooltip = "The health percentage at which to use Holy Shock", value = 88, min = 0, max = 100, step = 1, key = "holyShockThreshold" },
     { type = "entry", text = "Flash of Light Threshold", tooltip = "The health percentage at which to use Flash of Light", value = 88, min = 0, max = 100, step = 1, key = "flashOfLightThreshold" },
     { type = "separator" },
     { type = "entry", text = "Enable Offense", tooltip = "Enable or disable automatic offensive actions", enabled = true, key = "offense" },
+	{ type = "separator" },
+    { type = "entry", text = "Enable Beacon of Light", tooltip = "Enable or disable casting Beacon of Light", enabled = true, key = "beaconOfLightBuff" },
+    { type = "entry", text = "Enable Sacred Shield", tooltip = "Enable or disable casting Sacred Shield", enabled = true, key = "sacredShieldBuff" },
 }
 
 local function OnLoad()
@@ -60,28 +66,29 @@ local function OnUnLoad()
 end
 
 local queue = {
-	"auraMastery",
+    "auraMastery",
     "divineIllumination",
-	"divineShield",
+    "divineShield",
     "handOfProtection",
     "layOnHands",
-	"divineFavor",
+    "divineFavor",
     "holyShock",
     "flashOfLight",
     "offense",
-    "buffs",
+    "beaconOfLight",
+    "sacredShield",
 }
 
 local abilities = {
     ["layOnHands"] = function()
-        if enables["heal"] and ni.player.hp() <= values["layOnHandsThreshold"] and ni.spell.available(spells.layOnHands) and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") then
+        if enables["heal"] and ni.player.hp() <= values["layOnHandsThreshold"] and ni.spell.available(spells.layOnHands) and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") and not ni.spell.gcd() then
             ni.spell.stopcasting()
             ni.spell.cast(spells.layOnHands, 'player')
         end
     end,
-	
+
     ["divineShield"] = function()
-        if ni.player.hp() <= values["divineShieldThreshold"] and ni.spell.available(spells.divineShield) and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") then
+        if ni.player.hp() <= values["divineShieldThreshold"] and ni.spell.available(spells.divineShield) and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") and not ni.spell.gcd() then
             ni.spell.stopcasting()
             ni.spell.cast(spells.divineShield, 'player')
         end
@@ -96,12 +103,12 @@ local abilities = {
                 break
             end
         end
-        if allBelowThreshold and ni.spell.available(spells.handOfProtection) and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") then
+        if allBelowThreshold and ni.spell.available(spells.handOfProtection) and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") and not ni.spell.gcd() then
             ni.spell.stopcasting()
             ni.spell.cast(spells.handOfProtection, 'player')
         end
     end,
-	
+
     ["auraMastery"] = function()
         if enables["heal"] and ni.player.hp() <= values["auraMasteryThreshold"] and ni.spell.available(spells.auraMastery) and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") then
             ni.spell.cast(spells.auraMastery, 'player')
@@ -114,13 +121,13 @@ local abilities = {
             ni.spell.cast(spells.divineIllumination, 'player')
         end
     end,
-	
+
     ["divineFavor"] = function()
         if enables["heal"] and ni.player.hp() <= values["divineFavorThreshold"] and ni.spell.available(spells.divineFavor) and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") then
             ni.spell.cast(spells.divineFavor, 'player')
         end
     end,
-	
+
     ["holyShock"] = function()
         if enables["heal"] and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") then
             for i = 1, #ni.members do
@@ -147,13 +154,18 @@ local abilities = {
         -- Add offensive logic here
     end,
 
-    ["buffs"] = function()
-        if not ni.player.ismounted() and not UnitIsDeadOrGhost("player") then
-            if not ni.player.buff(53563) then
-                ni.spell.cast(53563, 'player')
+    ["beaconOfLight"] = function()
+        if enables["buffs"] and enables["beaconOfLightBuff"] and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") then
+            if not ni.player.buff(spells.beaconOfLight) then
+                ni.spell.cast(spells.beaconOfLight, 'player')
             end
-            if not ni.player.buff(53601) then
-                ni.spell.cast(53601, 'player')
+        end
+    end,
+
+    ["sacredShield"] = function()
+        if enables["buffs"] and enables["sacredShieldBuff"] and not ni.player.ismounted() and not UnitIsDeadOrGhost("player") then
+            if not ni.player.buff(spells.sacredShield) then
+                ni.spell.cast(spells.sacredShield, 'player')
             end
         end
     end,
