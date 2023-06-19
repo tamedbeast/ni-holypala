0bd9b05335a1f5fb6218ae93139ace9d9699eecf
nelbin4 <nelbinbinag@yahoo.com>
2023-06-19 17:07:26 +0800

    Update HolyPaladin.lua

--- HolyPaladin.lua
+++ HolyPaladin.lua
@@ -10,6 +10,7 @@ local queue = {
     "beaconOfLight",
     "sacredShield",
     "blessingOfKings",
+    "handOfFreedom",
 }
 
 local spells = {
@@ -24,12 +25,13 @@ local spells = {
     beaconOfLight = 53563,
     sacredShield = 53601,
     blessingOfKings = 20217,
+    handOfFreedom = 1044,
 }
 
 local values = {
     holyShockThreshold = 88,
     flashOfLightThreshold = 88,
-    divineFavorThreshold = 25,
+    divineFavorThreshold = 75,
     auraMasteryThreshold = 65,
     divineIlluminationThreshold = 65,
     divineShieldThreshold = 35,
@@ -49,6 +51,7 @@ local enables = {
     beaconOfLight = true,
     sacredShield = true,
     blessingOfKings = true,
+    handOfFreedom = true,
 }
 
 local function GUICallback(key, item_type, value)
@@ -67,27 +70,29 @@ local items = {
     callback = GUICallback,
     { type = "title", text = "Holy Paladin PvP Profile" },
     { type = "separator" },
-    { type = "entry", text = "Holy Shock Threshold", tooltip = "The health percentage at which to use Holy Shock", value = values.holyShockThreshold, min = 0, max = 100, step = 1, key = "holyShockThreshold" },
-    { type = "entry", text = "Flash of Light Threshold", tooltip = "The health percentage at which to use Flash of Light", value = values.flashOfLightThreshold, min = 0, max = 100, step = 1, key = "flashOfLightThreshold" },
-    { type = "entry", text = "Divine Favor Threshold", tooltip = "The health percentage at which to use Divine Favor", value = values.divineFavorThreshold, min = 0, max = 100, step = 1, key = "divineFavorThreshold" },
-    { type = "entry", text = "Aura Mastery Threshold", tooltip = "The health percentage at which to use Aura Mastery", value = values.auraMasteryThreshold, min = 0, max = 100, step = 1, key = "auraMasteryThreshold" },
-	{ type = "entry", text = "Divine Illumination Threshold", tooltip = "The mana percentage at which to use Divine Illumination", value = values.divineIlluminationThreshold, min = 0, max = 100, step = 1, key = "divineIlluminationThreshold" },
-    { type = "entry", text = "Divine Shield Threshold", tooltip = "The health percentage at which to use Divine Shield", value = values.divineShieldThreshold, min = 0, max = 100, step = 1, key = "divineShieldThreshold" },
-    { type = "entry", text = "Hand of Protection Threshold", tooltip = "The health percentage at which to use Hand of Protection", value = values.handOfProtectionThreshold, min = 0, max = 100, step = 1, key = "handOfProtectionThreshold" },
-    { type = "entry", text = "Lay on Hands Threshold", tooltip = "The health percentage at which to use Lay on Hands", value = values.layOnHandsThreshold, min = 0, max = 100, step = 1, key = "layOnHandsThreshold" },
+    { type = "entry", text = "Holy Shock", value = values.holyShockThreshold, min = 0, max = 100, step = 1, key = "holyShockThreshold" },
+    { type = "entry", text = "Flash of Light", value = values.flashOfLightThreshold, min = 0, max = 100, step = 1, key = "flashOfLightThreshold" },
+    { type = "entry", text = "Divine Favor", value = values.divineFavorThreshold, min = 0, max = 100, step = 1, key = "divineFavorThreshold" },
+    { type = "entry", text = "Aura Mastery", value = values.auraMasteryThreshold, min = 0, max = 100, step = 1, key = "auraMasteryThreshold" },
+    { type = "entry", text = "Divine Illumination", value = values.divineIlluminationThreshold, min = 0, max = 100, step = 1, key = "divineIlluminationThreshold" },
+    { type = "entry", text = "Divine Shield", value = values.divineShieldThreshold, min = 0, max = 100, step = 1, key = "divineShieldThreshold" },
+    { type = "entry", text = "Hand of Protection", value = values.handOfProtectionThreshold, min = 0, max = 100, step = 1, key = "handOfProtectionThreshold" },
+    { type = "entry", text = "Lay on Hands", value = values.layOnHandsThreshold, min = 0, max = 100, step = 1, key = "layOnHandsThreshold" },
     { type = "separator" },
-    { type = "entry", text = "Enable Lay on Hands", tooltip = "Enable or disable the use of Lay on Hands", enabled = enables.layOnHands, key = "layOnHands" },
-    { type = "entry", text = "Enable Holy Shock", tooltip = "Enable or disable the use of Holy Shock", enabled = enables.holyShock, key = "holyShock" },
-    { type = "entry", text = "Enable Flash of Light", tooltip = "Enable or disable the use of Flash of Light", enabled = enables.flashOfLight, key = "flashOfLight" },
-    { type = "entry", text = "Enable Divine Favor", tooltip = "Enable or disable the use of Divine Favor", enabled = enables.divineFavor, key = "divineFavor" },
-    { type = "entry", text = "Enable Aura Mastery", tooltip = "Enable or disable the use of Aura Mastery", enabled = enables.auraMastery, key = "auraMastery" },
-    { type = "entry", text = "Enable Divine Illumination", tooltip = "Enable or disable the use of Divine Illumination", enabled = enables.divineIllumination, key = "divineIllumination" },
-    { type = "entry", text = "Enable Divine Shield", tooltip = "Enable or disable the use of Divine Shield", enabled = enables.divineShield, key = "divineShield" },
-    { type = "entry", text = "Enable Hand of Protection", tooltip = "Enable or disable the use of Hand of Protection", enabled = enables.handOfProtection, key = "handOfProtection" },
-    { type = "entry", text = "Enable Beacon of Light", tooltip = "Enable or disable the use of Beacon of Light", enabled = enables.beaconOfLight, key = "beaconOfLight" },
-    { type = "entry", text = "Enable Sacred Shield", tooltip = "Enable or disable the use of Sacred Shield", enabled = enables.sacredShield, key = "sacredShield" },
-    { type = "entry", text = "Enable Blessing of Kings", tooltip = "Enable or disable the use of Blessing of Kings", enabled = enables.blessingOfKings, key = "blessingOfKings" },
+    { type = "entry", text = "Lay on Hands", enabled = enables.layOnHands, key = "layOnHands" },
+    { type = "entry", text = "Holy Shock", enabled = enables.holyShock, key = "holyShock" },
+    { type = "entry", text = "Flash of Light", enabled = enables.flashOfLight, key = "flashOfLight" },
+    { type = "entry", text = "Divine Favor", enabled = enables.divineFavor, key = "divineFavor" },
+    { type = "entry", text = "Aura Mastery", enabled = enables.auraMastery, key = "auraMastery" },
+    { type = "entry", text = "Divine Illumination", enabled = enables.divineIllumination, key = "divineIllumination" },
+    { type = "entry", text = "Divine Shield", enabled = enables.divineShield, key = "divineShield" },
+    { type = "entry", text = "Hand of Protection", enabled = enables.handOfProtection, key = "handOfProtection" },
+    { type = "entry", text = "Beacon of Light", enabled = enables.beaconOfLight, key = "beaconOfLight" },
+    { type = "entry", text = "Sacred Shield", enabled = enables.sacredShield, key = "sacredShield" },
+    { type = "entry", text = "Blessing of Kings", enabled = enables.blessingOfKings, key = "blessingOfKings" },
+    { type = "entry", text = "Hand of Freedom", enabled = enables.handOfFreedom, key = "handOfFreedom" },
 }
+
 
 local function OnLoad()
     ni.GUI.AddFrame("HolyPaladin", items)
@@ -96,39 +101,63 @@ end
 local function OnUnLoad()
     ni.GUI.DestroyFrame("HolyPaladin")
 end
+local crowdControlDebuffs = {
+    "Crippling Poison",
+    "Hamstring",
+    "Frostbite",
+    "Entangling Roots",
+    "Frost Nova",
+    "Frost Shock"
+}
+
+local function hasAnyDebuff(target, debuffList)
+    for _, debuffName in ipairs(debuffList) do
+        if target.debuff(debuffName) then
+            return true
+        end
+    end
+    return false
+end
 
 local function castSpell(spell, target, threshold)
-    if not ni.player.ismounted() and not UnitIsDeadOrGhost("player") and ni.spell.available(spell) and not ni.spell.gcd() then
-        local forbearance = 25771
-        if spell == spells.handOfProtection or spell == spells.divineShield or spell == spells.layOnHands then
-            if ni.player.debuff(forbearance) then
-                return
-            end
+    if ni.player.ismounted() or UnitIsDeadOrGhost("player") or not ni.spell.available(spell) or ni.spell.gcd() then
+        return
+    end
+
+    local forbearance = 25771
+    if spell == spells.handOfProtection or spell == spells.divineShield or spell == spells.layOnHands then
+        if ni.player.debuff(forbearance) then
+            return
         end
-        if target == 'player' then
-            if ni.player.hp() <= threshold then
-                ni.spell.stopcasting()
-                ni.spell.cast(spell, 'player')
-            end
-        else
-            for i = 1, #ni.members do
-                local ally = ni.members[i]
-                if ally:hp() <= threshold and ally:valid(spell, false, true) and ally:los() then
-                    if spell == spells.flashOfLight and ni.player.ismoving() then
-                        return
-                    end
-                    ni.spell.cast(spell, ally.guid)
-                end
+    end
+
+    if target == 'player' and ni.player.hp() <= threshold then
+        ni.spell.stopcasting()
+        ni.spell.cast(spell, 'player')
+        return
+    end
+
+    for i = 1, #ni.members do
+        local ally = ni.members[i]
+        if ally:valid(spell, false, true) and ally:los() and ally:hp() <= threshold and (spell ~= spells.flashOfLight or not ni.player.ismoving()) then
+            if spell == spells.handOfFreedom and hasAnyDebuff(ally, crowdControlDebuffs) then
+                ni.spell.cast(spell, ally.guid)
+            elseif spell ~= spells.handOfFreedom then
+                ni.spell.cast(spell, ally.guid)
             end
         end
     end
 end
 
-
-
 local function castSelfBuff(spell)
-    if not ni.player.ismounted() and not UnitIsDeadOrGhost("player") and ni.spell.available(spell) and not ni.spell.gcd() and not ni.player.buff(spell) then
-        ni.spell.cast(spell, 'player')
+    if ni.player.ismounted() or UnitIsDeadOrGhost("player") or not ni.spell.available(spell) or ni.spell.gcd() or ni.player.buff(spell) then
+        return
+    end
+
+    if spell == spells.handOfFreedom and hasAnyDebuff(ni.player, crowdControlDebuffs) then
+        ni.spell.cast(spell, "player")
+    elseif spell ~= spells.handOfFreedom then
+        ni.spell.cast(spell, "player")
     end
 end
 
@@ -144,6 +173,7 @@ local abilities = {
     beaconOfLight = function() if enables.beaconOfLight then castSelfBuff(spells.beaconOfLight) end end,
     sacredShield = function() if enables.sacredShield then castSelfBuff(spells.sacredShield) end end,
     blessingOfKings = function() if enables.blessingOfKings then castSelfBuff(spells.blessingOfKings) end end,
+    handOfFreedom = function() if enables.handOfFreedom then castSelfBuff(spells.handOfFreedom) end end,
 }
 
 ni.bootstrap.profile("HolyPaladin", queue, abilities, OnLoad, OnUnLoad)
